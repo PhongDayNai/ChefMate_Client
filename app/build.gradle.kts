cb88b7d343c05828db0691e965e9c1d49d3b9ef8
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-06-15 23:37:32 +0700

    update build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -40,12 +41,25 @@ android {
 }
 
 dependencies {
+    implementation("androidx.room:room-runtime:2.7.1")
+    implementation("androidx.room:room-ktx:2.7.1")
+    ksp("androidx.room:room-compiler:2.7.1")
+
+    implementation("androidx.navigation:navigation-compose:2.9.0")
+
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
     implementation("androidx.compose.material3:material3:1.4.0-alpha15")
     implementation("io.coil-kt:coil-compose:2.7.0")
 
     implementation("com.google.code.gson:gson:2.13.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    // Coil (Image Loading - recommended for loading Bitmaps in Compose)
+    implementation("io.coil-kt:coil-compose:2.4.0") // Hoặc phiên bản mới nhất
+
+    // Lấy ảnh từ thư viện
+    // AndroidX Activity library for registerForActivityResult
+    implementation("androidx.activity:activity-ktx:1.7.2")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -56,6 +70,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.runtime.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
