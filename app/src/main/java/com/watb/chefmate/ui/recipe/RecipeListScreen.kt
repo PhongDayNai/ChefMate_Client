be9b572efa151f46b871de3df431d1b771fc68cf
Dương Phong <dhphong266@gmail.com>
2025-06-16 16:59:24 +0700

    update UI Recipe Store

--- app/src/main/java/com/watb/chefmate/ui/recipe/RecipeListScreen.kt
+++ app/src/main/java/com/watb/chefmate/ui/recipe/RecipeListScreen.kt
@@ -1,15 +1,13 @@
 package com.watb.chefmate.ui.recipe
 
 
-import androidx.compose.foundation.Image
+import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -38,6 +36,7 @@ fun RecipeListScreen(navController: NavController, viewModel: RecipeViewModel) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFFFFFFF))
+            .safeDrawingPadding()
     ) {
         Header(
             text = "Kho công thức",
@@ -80,7 +79,6 @@ fun RecipeListScreen(navController: NavController, viewModel: RecipeViewModel) {
                 }
             }
         }
-
     }
 }
 
