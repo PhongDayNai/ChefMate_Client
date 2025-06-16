74bb91bd4f3e8cccc68998471284e4a09b16fe99
Dương Phong <dhphong266@gmail.com>
2025-06-16 23:28:37 +0700

    add interaction method & increase viewCount

--- app/src/main/java/com/watb/chefmate/api/ApiClient.kt
+++ app/src/main/java/com/watb/chefmate/api/ApiClient.kt
@@ -2,6 +2,10 @@ package com.watb.chefmate.api
 
 import android.util.Log
 import com.google.gson.Gson
+import com.watb.chefmate.data.CommentRequest
+import com.watb.chefmate.data.IncreaseRequest
+import com.watb.chefmate.data.InteractionResponse
+import com.watb.chefmate.data.LikeRequest
 import com.watb.chefmate.data.LoginResponse
 import com.watb.chefmate.data.RegisterRequest
 import com.watb.chefmate.data.TopTrendingResponse
@@ -74,6 +78,96 @@ object ApiClient {
                     }
                 }
             } catch (e: TimeoutException) {
+                e.printStackTrace()
+                null
+            }
+        }
+    }
+
+    suspend fun likeRecipe(userId: Int = 1, recipeId: Int): InteractionResponse? {
+        val likeRequest = LikeRequest(userId, recipeId)
+        val json = gson.toJson(likeRequest)
+
+        val requestBody = json.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+        val request = Request.Builder()
+            .url(ApiConstant.LIKE_URL)
+            .post(requestBody)
+            .build()
+
+        return withContext(Dispatchers.IO) {
+            try {
+                client.newCall(request).execute().use { response ->
+                    if (response.isSuccessful) {
+                        val responseBody = response.body?.string()
+                        responseBody?.let {
+                            gson.fromJson(it, InteractionResponse::class.java)
+                        }
+                    } else {
+                        Log.e("ApiClient", "Error: ${response.code}")
+                        null
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        }
+    }
+
+    suspend fun commentRecipe(userId: Int = 1, recipeId: Int, content: String): InteractionResponse? {
+        val commentRequest = CommentRequest(userId, recipeId, content)
+        val json = gson.toJson(commentRequest)
+
+        val requestBody = json.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+        val request = Request.Builder()
+            .url(ApiConstant.COMMENT_URL)
+            .post(requestBody)
+            .build()
+
+        return withContext(Dispatchers.IO) {
+            try {
+                client.newCall(request).execute().use { response ->
+                    if (response.isSuccessful) {
+                        val responseBody = response.body?.string()
+                        responseBody?.let {
+                            gson.fromJson(it, InteractionResponse::class.java)
+                        }
+                    } else {
+                        Log.e("ApiClient", "Error: ${response.code}")
+                        null
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        }
+    }
+
+    suspend fun increaseViewCount(recipeId: Int): InteractionResponse? {
+        val increaseRequest = IncreaseRequest(recipeId)
+        val json = gson.toJson(increaseRequest)
+
+        val requestBody = json.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+        val request = Request.Builder()
+            .url(ApiConstant.INCREASE_VIEW_COUNT_URL)
+            .post(requestBody)
+            .build()
+
+        return withContext(Dispatchers.IO) {
+            try {
+                client.newCall(request).execute().use { response ->
+                    if (response.isSuccessful) {
+                        val responseBody = response.body?.string()
+                        responseBody?.let {
+                            gson.fromJson(it, InteractionResponse::class.java)
+                            }
+                    } else {
+                        Log.e("ApiClient", "Error: ${response.code}")
+                        null
+                    }
+                }
+            } catch (e: Exception) {
                 e.printStackTrace()
                 null
             }
