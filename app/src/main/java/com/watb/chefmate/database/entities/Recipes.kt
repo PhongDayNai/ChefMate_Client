079aa9f84f25ee52f1313ddabe68e5f1b1687512
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2025-06-16 15:24:09 +0700

    update database

--- app/src/main/java/com/watb/chefmate/database/entities/Recipes.kt
+++ app/src/main/java/com/watb/chefmate/database/entities/Recipes.kt
@@ -3,17 +3,35 @@ package com.watb.chefmate.database.entities
 import android.graphics.Bitmap
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.watb.chefmate.data.Recipe
 
 @Entity(tableName = "Recipes")
 data class RecipeEntity(
     @PrimaryKey(autoGenerate = true) val recipeId: Int = 0,
     val recipeName: String,
-    val image: Bitmap,
+    val image: String,
     val userName: String,
     val isPublic: Boolean,
     val likeQuantity: Int,
     val cookTime: String,
     val ration: Int,
     val viewCount: Int,
+    val ingredients: String,
+    val cookingSteps: String,
     val createdAt: String
 )
+
+fun RecipeEntity.toRecipe(): Recipe {
+    return Recipe(
+        recipeId = recipeId,
+        image = image,
+        name = recipeName,
+        author = userName,
+        likesQuantity = likeQuantity,
+        userViews = viewCount,
+        ingredients = ingredients.split(";;;"), // hoặc map từ Relation nếu có
+        cookingSteps = cookingSteps.split(";;;"), // hoặc map từ Relation nếu có
+        cookingTime = cookTime,
+        comments = emptyList() // hoặc map từ relation nếu có
+    )
+}
