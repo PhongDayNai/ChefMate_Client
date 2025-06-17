c3f0b8105ea2c43977c9bfae202619140231e12f
Dương Phong <dhphong266@gmail.com>
2025-06-17 19:06:22 +0700

    add ProfileScreen

--- app/src/main/java/com/watb/chefmate/data/AppConstant.kt
+++ app/src/main/java/com/watb/chefmate/data/AppConstant.kt
@@ -11,4 +11,21 @@ object AppConstant {
             1.0f to Color(0xFFFDBA74)
         )
     )
+
+    val backgroundGradient = Brush.verticalGradient(
+        colorStops = arrayOf(
+            0.0f to Color(0xFFF97316),
+            0.075f to Color(0xFFFB923C),
+            0.225f to Color(0xFFFDBA74),
+            1.0f to Color(0xFFFDBA74)
+        )
+    )
+
+    val backgroundProfileGradient = Brush.verticalGradient(
+        colorStops = arrayOf(
+            0.0f to Color(0xFFF97316),
+            0.5f to Color(0xFFFDBA74),
+            1.0f to Color(0xFFFDBA74)
+        )
+    )
 }
