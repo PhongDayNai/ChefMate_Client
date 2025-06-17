f63cf56ab29ede2e305fd0dcf0af3f215afde049
Dương Phong <dhphong266@gmail.com>
2025-06-17 12:05:29 +0700

    add createRecipe Api

--- app/src/main/java/com/watb/chefmate/api/ApiClient.kt
+++ app/src/main/java/com/watb/chefmate/api/ApiClient.kt
@@ -1,20 +1,32 @@
 package com.watb.chefmate.api
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
+import android.provider.MediaStore
 import android.util.Log
+import androidx.core.net.toUri
 import com.google.gson.Gson
 import com.watb.chefmate.data.CommentRequest
+import com.watb.chefmate.data.CreateRecipeData
+import com.watb.chefmate.data.CreateRecipeResponse
 import com.watb.chefmate.data.IncreaseRequest
 import com.watb.chefmate.data.InteractionResponse
 import com.watb.chefmate.data.LikeRequest
 import com.watb.chefmate.data.LoginResponse
+import com.watb.chefmate.data.Recipe
 import com.watb.chefmate.data.RegisterRequest
 import com.watb.chefmate.data.TopTrendingResponse
+import com.watb.chefmate.helper.CommonHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.ByteArrayOutputStream
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
@@ -78,6 +90,60 @@ object ApiClient {
                     }
                 }
             } catch (e: TimeoutException) {
+                e.printStackTrace()
+                null
+            }
+        }
+    }
+
+    suspend fun createRecipe(context: Context, recipe: CreateRecipeData): CreateRecipeResponse? {
+        val bitmap = if (Build.VERSION.SDK_INT < 28) {
+            MediaStore.Images.Media.getBitmap(context.contentResolver, recipe.image.toUri())
+        } else {
+            val source = ImageDecoder.createSource(context.contentResolver, recipe.image.toUri())
+            ImageDecoder.decodeBitmap(source)
+        }
+
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val byteArray = byteArrayOutputStream.toByteArray()
+
+        val imageRequestBody = byteArray.toRequestBody("image/jpeg".toMediaTypeOrNull(), 0, byteArray.size)
+        val ingredientsRequestBody = gson.toJson(recipe.ingredients)
+        val cookingSteps = gson.toJson(recipe.cookingSteps)
+        Log.d("ApiClient", "Ingredients Request Body: $ingredientsRequestBody")
+        Log.d("ApiClient", "Cooking Steps: $cookingSteps")
+
+        val multipartBody = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart("recipeName", recipe.recipeName)
+            .addFormDataPart("cookingTime", recipe.cookingTime)
+            .addFormDataPart("ration", "${recipe.ration}")
+            .addFormDataPart("ingredients", ingredientsRequestBody)
+            .addFormDataPart("cookingSteps", cookingSteps)
+            .addFormDataPart("userId", "1")
+            .addFormDataPart("image", "${CommonHelper.parseName(recipe.recipeName)}.jpg", imageRequestBody)
+            .build()
+
+        val request = Request.Builder()
+            .url(ApiConstant.CREATE_RECIPE_URL)
+            .post(multipartBody)
+            .build()
+
+        return withContext(Dispatchers.IO) {
+            try {
+                client.newCall(request).execute().use { response ->
+                    if (response.isSuccessful) {
+                        val responseBody = response.body?.string()
+                        responseBody?.let {
+                            gson.fromJson(it, CreateRecipeResponse::class.java)
+                        }
+                    } else {
+                        Log.e("ApiClient", "Error: ${response.code}")
+                        null
+                    }
+                }
+            } catch (e: Exception) {
                 e.printStackTrace()
                 null
             }
