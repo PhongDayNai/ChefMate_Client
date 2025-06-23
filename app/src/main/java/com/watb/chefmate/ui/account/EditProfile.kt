38eb3fe2af68ca29db7c908d71b56e8e9ff60c29
Hoangthduong <duongnguk1@gmail.com>
2025-06-23 13:14:47 +0700

    add edit profile

---
+++ app/src/main/java/com/watb/chefmate/ui/account/EditProfile.kt
@@ -0,0 +1,19 @@
+package com.watb.chefmate.ui.account
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun EditProfileScreen(
+    navController: NavController
+) {
+
+}
+
+@Composable
+fun EditPersonalInformation(
+
+) {
+
+}
+
