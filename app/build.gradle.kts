5fa36df1e17f7a7c516c024ae09d0b8b11dc8940
Dương Hùng Phong <dhphong266@gmail.com>
2026-04-10 23:19:03 +0700

    build(android): bump version to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.watb.chefmate"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.0.0"
+        versionCode = 4
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "API_BASE_URL", apiBaseUrl.asBuildConfigString())
